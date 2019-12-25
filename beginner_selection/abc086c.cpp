5e95e9b7da067a8d7f42328f4b135350ad1501f0
c-bata <contact@c-bata.link>
2019-12-25 18:49:46 +0900

    abc086c: pass

--- beginner_selection/abc086c.cpp
+++ beginner_selection/abc086c.cpp
@@ -22,17 +22,18 @@ int main() {
     int n;
     cin >> n;
 
-    int t[n], x[n], y[n];
-    for (int i=0; i<n; i++) {
+    int t[n+1], x[n+1], y[n+1];
+    // 考え方はOKだったけど、最後落ちた理由は (0, 0) から始まるという前提が抜けていた。
+    // 凡ミスもあり3回失敗で15分のペナルティ.
+    t[0] = 0;
+    x[0] = 0;
+    y[0] = 0;
+    for (int i=1; i<n+1; i++) {
         cin >> t[i] >> x[i] >> y[i];
     }
 
     for (int i=0; i<n; i++) {
-        if (i == 0) {
-            continue;
-        }
-
-        if (!check(t[i-1], t[i], x[i-1], x[i], y[i-1], y[i])) {
+        if (!check(t[i], t[i+1], x[i], x[i+1], y[i], y[i+1])) {
             cout << "No" << endl;
             return 0;
         }
