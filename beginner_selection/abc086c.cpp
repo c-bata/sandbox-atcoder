09778515ceb287539c9ab30eb4d9187c0bcef9cf
c-bata <contact@c-bata.link>
2019-12-25 18:40:50 +0900

    abc086c: almost passed, but fail 1

--- beginner_selection/abc086c.cpp
+++ beginner_selection/abc086c.cpp
@@ -6,20 +6,16 @@ using namespace std;
 
 bool check(int prev_t, int t, int prev_x, int x, int prev_y, int y) {
     int diff_t = t-prev_t;
-    //int diff_x = abs(prev_x - x);
-    //int diff_y = abs(prev_y - y);
+    int diff_x = abs(prev_x - x);
+    int diff_y = abs(prev_y - y);
 
-    if (diff_t == 1) {
-        if (abs(x - prev_x) == 1 && y-prev_y == 0) return true;
-        if (abs(y - prev_y) == 1 && x-prev_x == 0) return true;
+    if (diff_t == diff_x + diff_y) {
+        return true;
+    } else if (diff_t < diff_x + diff_y) {
         return false;
     }
 
-    if (check(prev_t+1, t, prev_x+1, x, prev_y, y)) return true;
-    if (check(prev_t+1, t, prev_x-1, x, prev_y, y)) return true;
-    if (check(prev_t+1, t, prev_x, x, prev_y+1, y)) return true;
-    if (check(prev_t+1, t, prev_x, x, prev_y-1, y)) return true;
-    return false;
+    return (diff_x + diff_y - diff_t) % 2 == 0;
 }
 
 int main() {
