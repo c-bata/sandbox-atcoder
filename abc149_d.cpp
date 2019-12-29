dcf099b18b621a03f8bc936391e716751a2ea355
c-bata <contact@c-bata.link>
2019-12-29 21:18:13 +0900

    Implement abc149_d

--- abc149_d.cpp
+++ abc149_d.cpp
@@ -13,37 +13,42 @@ int main() {
 
     int total_point = 0;
     for (int diff_k=0; diff_k<K; diff_k++) {
-
         int r_cnt = 0, s_cnt = 0, p_cnt = 0;
+
+        char prev_win = false;
         for (int i=N-1-diff_k; i>=0; i-=K) {
-            cout << "iは？" << diff_k << " " << i << endl;
+
             char hand = t[i];
+            int next_index = i + K;
+            // 後ろのindexが必ず最善の手をとるようにする。k個前が同じ手が最善でも必ず最善を取る。
+
             if (hand == 'r') {
-                r_cnt++;
+                if (prev_win && (next_index < N && t[next_index] == 'r')) {
+                    prev_win = false;
+                } else {
+                    r_cnt++;
+                    prev_win = true;
+                }
             } else if (hand == 'p') {
-                p_cnt++;
+                if (prev_win && (next_index < N && t[next_index] == 'p')) {
+                    prev_win = false;
+                } else {
+                    p_cnt++;
+                    prev_win = true;
+                }
             } else if (hand == 's') {
-                s_cnt++;
+                if (prev_win && (next_index < N && t[next_index] == 's')) {
+                    prev_win = false;
+                } else {
+                    s_cnt++;
+                    prev_win = true;
+                }
             }
         }
 
-        int max_hand = 0;
-        int point = 0;
-        if (r_cnt > max_hand) {
-            max_hand = r_cnt;
-            point = P;
-        }
-        if (p_cnt >= max_hand && S > point) {
-            max_hand = p_cnt;
-            point = S;
-        }
-        if (s_cnt >= max_hand && R > point) {
-            max_hand = s_cnt;
-            point = R;
-        }
-
-        total_point += max_hand * point;
-        cout << total_point << " " << max_hand << " " << point << endl;
+        total_point += r_cnt * P;
+        total_point += p_cnt * S;
+        total_point += s_cnt * R;
     }
 
     cout << total_point << endl;
