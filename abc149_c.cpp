cec52b6f22a2c6642c367c09415adb050dd64f30
c-bata <contact@c-bata.link>
2019-12-29 20:19:51 +0900

    Add abc149_c

--- abc149_c.cpp
+++ abc149_c.cpp
@@ -3,15 +3,32 @@
 
 using namespace std;
 
-bool is_prime(int x) {
-    return false;
+bool is_prime(int n) {
+    switch(n){
+        case 0: // fall-through
+        case 1: return false;
+        case 2: return true;
+    } // n > 2 が保証された
+
+    if(n%2 == 0) return false;
+
+    // 上で i=2 相当は調べたので、i=3から奇数のみ調べる
+    for(unsigned i=3;i*i<=n;i+=2){
+        if(n%i == 0) return false;
+    }
+
+    return true;
 }
 
 int main() {
     int x;
     cin >> x;
 
-    int next_prime = 0;
-
-    cout << next_prime << endl;
+    if (x != 2 && x % 2 == 0) x+=1;
+    for (int i = x; ; i+=2) {
+        if (is_prime(i)) {
+            cout << i << endl;
+            return 0;
+        }
+    }
 }
