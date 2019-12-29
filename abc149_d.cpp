38af0129ce0e5450582c5f0722f6660614606f55
c-bata <contact@c-bata.link>
2019-12-29 20:49:52 +0900

    wip with bug

--- abc149_d.cpp
+++ abc149_d.cpp
@@ -11,5 +11,40 @@ int main() {
     cin >> R >> S >> P;
     cin >> t;
 
-    cout << N + K + R + S + P << " " << t << endl;
+    int total_point = 0;
+    for (int diff_k=0; diff_k<K; diff_k++) {
+
+        int r_cnt = 0, s_cnt = 0, p_cnt = 0;
+        for (int i=N-1-diff_k; i>=0; i-=K) {
+            cout << "iは？" << diff_k << " " << i << endl;
+            char hand = t[i];
+            if (hand == 'r') {
+                r_cnt++;
+            } else if (hand == 'p') {
+                p_cnt++;
+            } else if (hand == 's') {
+                s_cnt++;
+            }
+        }
+
+        int max_hand = 0;
+        int point = 0;
+        if (r_cnt > max_hand) {
+            max_hand = r_cnt;
+            point = P;
+        }
+        if (p_cnt >= max_hand && S > point) {
+            max_hand = p_cnt;
+            point = S;
+        }
+        if (s_cnt >= max_hand && R > point) {
+            max_hand = s_cnt;
+            point = R;
+        }
+
+        total_point += max_hand * point;
+        cout << total_point << " " << max_hand << " " << point << endl;
+    }
+
+    cout << total_point << endl;
 }
---
+++ abc149_d.py
@@ -0,0 +1,4 @@
+def main():
+
+    a, b = input().strip().split()
+
