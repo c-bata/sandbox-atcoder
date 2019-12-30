8adc7bf6d813ad1737a0ce3435e4b4a2b1bd8702
c-bata <contact@c-bata.link>
2019-12-30 13:48:03 +0900

    add abc138 d

--- abc138_d.cpp
+++ abc138_d.cpp
@@ -1,14 +1,60 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
 int main() {
-    int a, b, c;
-    string s;
+    int n;
+    cin >> n;
+    int a[n+1];
+    int b[n+1];
+    memset(b, 0, sizeof(b));
 
-    cin >> a >> b >> c;
-    cin >> s;
+    a[0] = 0;
+    for (int i=1; i<=n; i++)
+        cin >> a[i];
 
-    cout << a+b+c << " " << s << endl;
+    for (int i=n; i>=1; i--) {
+        int num = 0;
+        for (int x=i; x<=n; x+=i) {
+            num += b[x];
+        }
+        if (num % 2 == a[i]) {
+            b[i] = 0;
+        } else {
+            b[i] = 1;
+        }
+    }
+
+    int sum = 0;
+    for (int i=1; i<=n; i++)
+        sum += b[i];
+
+    if (b[1] == sum % 2) {
+        cout << sum << endl;
+        bool first = true;
+        for (int i=1; i<=n; i++) {
+            if (b[i] == 1) {
+                if (!first) {
+                    cout << " ";
+                } else {
+                    first = false;
+                }
+                cout << i;
+            }
+        }
+        if (!first) {
+            cout << endl;
+        }
+
+        //for (int i=1; i<=n; i++) {
+        //    if (i != 1)
+        //        cout << " ";
+        //    cout << b[i];
+        //}
+        //cout << endl;
+    } else {
+        cout << "-1" << endl;
+    }
 }
--- abc138_d_test.sh
+++ abc138_d_test.sh
@@ -5,6 +5,16 @@ set -ex
 g++ -o ./build/abc138_d -Wall -O2 -std=c++14 ./abc138_d.cpp
 
 ./build/abc138_d <<EOF
-1 2 3
-foo
+3
+1 0 0
+EOF
+
+./build/abc138_d <<EOF
+10
+1 0 0 1 1 1 0 0 1 1
+EOF
+
+./build/abc138_d <<EOF
+5
+0 0 0 0 0
 EOF
