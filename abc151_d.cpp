96931c1cf7dc64e4f92d515e1c9c784861ae6b94
c-bata <contact@c-bata.link>
2020-01-13 00:55:57 +0900

    add d by watching youtube live

--- abc151_d.cpp
+++ abc151_d.cpp
@@ -1,91 +1,65 @@
 #include <iostream>
 #include <string>
+#include <queue>
+
+// https://youtu.be/1oLDDdWRu6Y の回答をみて写経。
+
+#define rep(i, n) for (int i=0; i< (n); ++i)
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 
 using namespace std;
 
-int H, W;
-string Sij[21];
-int dp[21][21][21][21];
-
-template<class T> inline bool chmin(T& a, T b) {
-    if (a > b) {
-        a = b;
-        return true;
-    }
-    return false;
-}
-
-template<class T> inline bool chmax(T& a, T b) {
-    if (a < b) {
-        a = b;
-        return true;
-    }
-    return false;
-}
-
-int dist(int h1, int h2, int w1, int w2) {
-    if (dp[h1][h2][w1][w2] != -1) {
-        return dp[h1][h2][w1][w2];
-    }
-
-    if (h1 < 0) {
-        return -2;
-    } else if (h2>H) {
-        return -2;
-    } else if (w1 < 0) {
-        return -2;
-    } else if (w2 > W) {
-        return -2;
-    }
-
-    if (Sij[h1][w1] == '#' || Sij[h2][w2] == '#') {
-        return -2;
-    }
-
-    if (h1 == h2 && w1 == w2) {
-        return 0;
-    }
-
-    if (abs(h1-h2) == 1 && w1 == w2) {
-        return 1;
-    } else if (h1 == h2 && abs(w1-w2) == 1) {
-        return 1;
-    }
-
-    if (h1 > 1) {
-        chmin(dp[h1-1][h2][w1][w2], dist(h1-1, h2, w1, w2) + 1);
-    }
-    if (h2 < H-1) {
-        chmin(dp[h1][h2+1][w1][w2], dist(h1, h2+1, w1, w2) + 1);
-    }
-    if (w1 > 1) {
-        chmin(dp[h1][h2][w1-1][w2], dist(h1, h2, w1-1, w2) + 1);
-    }
-    if (w2 < W-1) {
-        chmin(dp[h1][h2][w1][w2+1], dist(h1, h2, w1, w2+1) + 1);
-    }
-    return 0;
-}
+typedef pair<int, int> P;
+const int di[] = {-1, 0, 1, 0};
+const int dj[] = {0, -1, 0, 1};
 
 int main() {
+    int H, W;
+    int ans = 0;
     cin >> H >> W;
 
-    for (int i=0; i<H; i++) {
-        cin >> Sij[i];
-    }
-    memset(dp, -1, sizeof(dp));
+    string sij[H];
+    rep(i, H) cin >> sij[i];
 
-    int maxValue = -1;
-    for (int h=0; h<H; h++) {
-        for (int h2=h; h2<H; h2++) {
-            for (int w = 0; w < W; w++) {
-                for (int w2 = w; w2 < W; w2++) {
-                    if ((h != h2) && (w != w2)) {
-                        chmax(maxValue, dist(h, h2, w, w2));
-                    }
+    rep(si, H) {
+        rep(sj, W) {
+            if (sij[si][sj] == '#')
+                continue;
+
+            vector<vector<int>> dist(H, vector<int>(W, -1));
+            queue<P> q;
+            auto update = [&](int i, int j, int x) {  // マス i, j の値を x で上書き
+                if (dist[i][j] != -1)
+                    return;
+                dist[i][j] = x;
+                q.push(P(i, j));
+            };
+
+            update(si, sj, 0);
+
+            while (!q.empty()) {
+                int i = q.front().first;
+                int j = q.front().second;
+                q.pop();  // これを忘れないこと！
+
+                rep(dir, 4) {
+                    int ni = i + di[dir], nj = j+dj[dir];
+                    if (ni < 0 || ni >= H || nj < 0 || nj >= W)
+                        continue;
+                    if (sij[ni][nj] == '#')
+                        continue;
+                    update(ni, nj, dist[i][j] + 1);
+                }
+            }
+
+            rep(i, H) {
+                rep(j, W) {
+                    chmax(ans, dist[i][j]);
                 }
             }
         }
     }
-    cout << maxValue << endl;
+    cout << ans << endl;
 }
--- abc151_d_test.sh
+++ abc151_d_test.sh
@@ -10,3 +10,10 @@ g++ -o ./build/abc151_d -Wall -O2 -std=c++14 ./abc151_d.cpp
 ...
 ...
 EOF
+
+./build/abc151_d <<EOF
+3 5
+...#.
+.#.#.
+.#...
+EOF
