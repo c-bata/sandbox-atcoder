892f5e60d1e5001c083f93cf4766d92c82cdfdc2
c-bata <contact@c-bata.link>
2020-01-12 21:09:36 +0900

    b

--- abc151_b.cpp
+++ abc151_b.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main() {
-    int a, b, c;
-    string s;
+    int N, K, M;
+    cin >> N >> K >> M;
 
-    cin >> a >> b >> c;
-    cin >> s;
+    int A[N];
 
-    cout << a+b+c << " " << s << endl;
+    int sum = 0;
+    for (int i=0; i<N-1; i++) {
+        cin >> A[i];
+        sum += A[i];
+    }
+
+    if (sum + K >= N*M) {
+        cout << max(N*M - sum, 0) << endl;
+    } else {
+        cout << "-1" << endl;
+    }
 }
