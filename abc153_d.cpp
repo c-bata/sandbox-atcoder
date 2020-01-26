d5918d2def7aa7cb571b27680e96b60561340b1e
c-bata <contact@c-bata.link>
2020-01-26 22:10:17 +0900

    d

--- abc153_d.cpp
+++ abc153_d.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <stack>
+#include <math.h>
 
 #define rep(i, n) for (int i=0; i< (n); ++i)
 typedef long long ll;
@@ -9,12 +10,22 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; }
 
 using namespace std;
 
+ll count_num2(ll h) {
+/*
+ * def a(n):
+ *     return 0 if n==0 else 1 + 2*a(int(n/2))
+ */
+    if (h == 0) {
+        return 0;
+    }
+    ll next = floor(h / 2);
+    return 1 + 2 * count_num2(next);
+}
+
 int main() {
-    int a, b, c;
-    string s;
+    ll d;
+    cin >> d;
+    //s.push(d);
 
-    cin >> a >> b >> c;
-    cin >> s;
-
-    cout << a+b+c << " " << s << endl;
+    cout << count_num2(d) << endl;
 }
