0462ac45b4b7a71a3eba0c8d822b1490ede627da
c-bata <contact@c-bata.link>
2020-02-09 21:39:35 +0900

    d

--- abc154_d.cpp
+++ abc154_d.cpp
@@ -10,12 +10,50 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; }
 
 using namespace std;
 
+double kitaichi(int *p, int k, int pos) {
+    double max_kitaichi = 0;
+    for (int i=pos; i<pos+k; i++) {
+        double souwa = (double) (p[i] * (p[i] + 1)) / 2;
+        double bunbo = p[i];
+        double kitaichi = souwa / bunbo;
+        max_kitaichi += kitaichi;
+    }
+    return max_kitaichi;
+}
+
 int main() {
-    int a, b, c;
-    string s;
+    int n, k;
 
-    cin >> a >> b >> c;
-    cin >> s;
+    cin >> n >> k;
 
-    cout << a+b+c << " " << s << endl;
+    int p[n];
+    rep(i, n) cin >> p[i];
+
+    int sum = 0;
+    rep(i, k) {
+        sum += p[i];
+    }
+    int max_pos = 0;
+    int max_sum = sum;
+
+    rep(i, n-k-1) {
+        //cout << i << " " << sum << " " << kitaichi(p, k, i) << endl;
+        sum = sum - p[i] + p[i+k];
+        if (max_sum < sum) {
+            max_sum = sum;
+            max_pos = i+1;
+        }
+    }
+
+    //cout << max_pos << endl;
+
+    double max_kitaichi = 0;
+    for (int i=max_pos; i<max_pos+k; i++) {
+        double souwa = (double) (p[i] * (p[i] + 1)) / 2;
+        double bunbo = p[i];
+        double kitaichi = souwa / bunbo;
+        max_kitaichi += kitaichi;
+    }
+
+    cout << max_kitaichi << endl;
 }
--- abc154_d_test.sh
+++ abc154_d_test.sh
@@ -5,6 +5,17 @@ set -ex
 g++ -o ./build/abc154_d -Wall -O2 -std=c++14 ./abc154_d.cpp
 
 ./build/abc154_d <<EOF
-1 2 3
-foo
+5 3
+1 2 2 4 5
+EOF
+
+
+./build/abc154_d <<EOF
+4 1
+6 6 6 6
+EOF
+
+./build/abc154_d <<EOF
+10 4
+17 13 13 12 15 20 10 13 17 11
 EOF
