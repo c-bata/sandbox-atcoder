de192b4facbacca37f79b4803e3eab05b76361db
c-bata <contact@c-bata.link>
2020-02-16 22:19:40 +0900

    d muri

--- abc155_d.cpp
+++ abc155_d.cpp
@@ -11,11 +11,58 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; }
 using namespace std;
 
 int main() {
-    int a, b, c;
-    string s;
+    ll n, k;
+    cin >> n >> k;
 
-    cin >> a >> b >> c;
-    cin >> s;
+    ll a[n];
+    ll abs[n];
+    int sign[n];
 
-    cout << a+b+c << " " << s << endl;
+    int sign_plus_num = 0;
+    int sign_minus_num = 0;
+    int sign_zero_num = 0;
+
+    vector<int> plus_a(0);
+    vector<int> minus_a(0);
+
+    rep(i, n) {
+        cin >> a[i];
+
+        abs[i] = llabs(a[i]);
+
+        if (a[i] == 0) {
+            sign[i] = 0;
+            sign_zero_num++;
+        } else if (a[i] > 0) {
+            sign[i] = 1;
+            sign_plus_num++;
+            plus_a.push_back(a[i]);
+        } else {
+            sign[i] = -1;
+            sign_minus_num++;
+            minus_a.push_back(a[i]);
+        }
+    }
+
+    sort(abs, abs+n, greater<>());
+    sort(plus_a.begin(), plus_a.end(), greater<>());
+    sort(minus_a.begin(), minus_a.end(), less<>());
+
+    int minus_kumiawase = sign_plus_num * sign_minus_num;
+    int zero_kumiawase = sign_zero_num * (n - sign_zero_num);
+    if (k < minus_kumiawase) {
+        // 負の数の中に正解がある
+        // ここの計算にはDPが必要そう
+    } else if (k < minus_kumiawase + zero_kumiawase) {
+        cout << 0 << endl;
+    } else {
+        // 正の数の中に正解がある。
+        // ここの計算にはDPが必要そう
+    }
+
+    rep(i, n) cout << abs[i] << endl;
+    cout << "foo" << endl;
+    rep(i, plus_a.size()) cout << plus_a[i] << endl;
+    cout << "foo" << endl;
+    rep(i, minus_a.size()) cout << minus_a[i] << endl;
 }
--- abc155_d_test.sh
+++ abc155_d_test.sh
@@ -5,6 +5,11 @@ set -ex
 g++ -o ./build/abc155_d -Wall -O2 -std=c++14 ./abc155_d.cpp
 
 ./build/abc155_d <<EOF
-1 2 3
-foo
+4 3
+3 3 -4 -2
+EOF
+
+./build/abc155_d <<EOF
+10 40
+5 4 3 2 -1 0 0 0 0 0
 EOF
