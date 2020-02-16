e214fd7f62e580af5088c8a491bb88d01b60e90b
c-bata <contact@c-bata.link>
2020-02-16 21:47:24 +0900

    c

--- abc155_c.cpp
+++ abc155_c.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <map>
+#include <cstring>
 
 #define rep(i, n) for (int i=0; i< (n); ++i)
 typedef long long ll;
@@ -11,11 +13,49 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; }
 using namespace std;
 
 int main() {
-    int a, b, c;
-    string s;
+    int n;
+    cin >> n;
 
-    cin >> a >> b >> c;
-    cin >> s;
+    //string s[n];
+    //rep(i, n) cin >> s[i];
 
-    cout << a+b+c << " " << s << endl;
+    map<string, int> m{};
+    rep(i, n) {
+        string a;
+        cin >> a;
+        if (m.count(a) == 0) {
+            m[a] = 1;
+        } else {
+            m[a]++;
+        }
+    }
+
+
+    string items[m.size()];
+    pair<string, int> map_pair[m.size()];
+    int i = 0;
+    for (auto const& x : m) {
+        items[i] = x.first;
+        map_pair[i] = x;
+        i++;
+    }
+
+
+    sort(map_pair, map_pair+m.size(), [](pair<string, int> x, pair<string, int> y) {
+        if (x.second == y.second) {
+            return strcmp(x.first.c_str(), y.first.c_str()) > 0;
+        }
+        return x.second > y.second;
+    });
+
+    //rep(j, m.size()) cout << map_pair[j].first << " " << map_pair[j].second << endl;
+
+    int prev = -1;
+    rep(j, m.size()) {
+        if (j != 0 && map_pair[j].second != prev) {
+            return 0;
+        }
+        cout << map_pair[j].first << endl;
+        prev = map_pair[j].second;
+    }
 }
--- abc155_c_test.sh
+++ abc155_c_test.sh
@@ -5,6 +5,43 @@ set -ex
 g++ -o ./build/abc155_c -Wall -O2 -std=c++14 ./abc155_c.cpp
 
 ./build/abc155_c <<EOF
-1 2 3
-foo
+7
+beat
+vet
+beet
+bed
+vet
+bet
+beet
+EOF
+
+./build/abc155_c <<EOF
+8
+buffalo
+buffalo
+buffalo
+buffalo
+buffalo
+buffalo
+buffalo
+buffalo
+EOF
+
+./build/abc155_c <<EOF
+7
+bass
+bass
+kick
+kick
+bass
+kick
+kick
+EOF
+
+./build/abc155_c <<EOF
+4
+ushi
+tapu
+nichia
+kun
 EOF
