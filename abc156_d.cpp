54fea13979133e442c9a6485e68b5e541645e936
c-bata <contact@c-bata.link>
2020-02-22 21:50:09 +0900

    d wip

--- abc156_d.cpp
+++ abc156_d.cpp
@@ -10,12 +10,59 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; }
 
 using namespace std;
 
+int calcNumOfCombination(int n, int r){
+    int num = 1;
+    for(int i = 1; i <= r; i++){
+        num = num * (n - i + 1) / i;
+    }
+    return num;
+}
+
 int main() {
-    int a, b, c;
-    string s;
+    ll n, a, b;
 
-    cin >> a >> b >> c;
-    cin >> s;
+    cin >> n >> a >> b;
 
-    cout << a+b+c << " " << s << endl;
+    ll zenbu = 0;
+    ll prev = -1;
+    ll combi = 0;
+    for (int r=1; r<=n; r++) {
+        if (prev == -1) {
+            combi = n;
+        } else {
+            ll bunsi = (ll)(n - r + 1);
+
+            ll prev_tmp = prev;
+            for (;;) {
+                if (prev_tmp > 1000000000 + 7) {
+                    prev_tmp %= 1000000000 + 7;
+                } else {
+                    break;
+                }
+            }
+
+            combi = prev_tmp * bunsi / (ll)r;
+        }
+        cout << "debug: " << r << " " << combi << " " << calcNumOfCombination(n, r) << endl;
+        prev = combi;
+
+        if (combi > 922337203685477580) {
+            cout << "overflow: " << r << " " << combi << endl;
+        }
+
+        if (r == a || r == b) {
+            continue;
+        }
+
+        zenbu += (ll) combi;
+        for (;;) {
+            if (zenbu > 1000000000 + 7) {
+                zenbu %= 1000000000 + 7;
+            } else {
+                break;
+            }
+        }
+    }
+
+    cout << zenbu << endl;
 }
--- abc156_d_test.sh
+++ abc156_d_test.sh
@@ -9,5 +9,9 @@ g++ -o ./build/abc156_d -Wall -O2 -std=c++14 ./abc156_d.cpp
 EOF
 
 ./build/abc156_d <<EOF
-1000000000 141421 173205
+5 1 3
 EOF
+
+#./build/abc156_d <<EOF
+#1000000000 141421 173205
+#EOF
